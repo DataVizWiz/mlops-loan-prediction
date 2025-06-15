b53d82e066d81970ac780e5cc5a89199567bb0d0
DataVizWiz <metin@alisho.co.uk>
2025-06-15 17:41:25 +0100

    Project module setup

--- push.ps1
+++ push.ps1
@@ -2,8 +2,6 @@ param (
     [string]$commMess
 )
 
-Remove-Item src/mlops_loan_default_predictions.egg-info -Recurse -Force
-pip freeze > requirements.dev.txt
 black .
 git add .
 git commit -m $commMess
