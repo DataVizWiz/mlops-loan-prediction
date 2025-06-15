5375c7f5049bc88af56e5c842353558bd4811ac3
DataVizWiz <metin@alisho.co.uk>
2025-06-15 20:47:03 +0100

    Project module setup

--- push.ps1
+++ push.ps1
@@ -2,6 +2,7 @@ param (
     [string]$commMess
 )
 
+pipdeptree --freeze --warn silence > .\requirements.dev.txt
 black src
 git add .
 git commit -m $commMess
--- scripts/ps/Install-Poetry.ps1
+++
@@ -1,7 +0,0 @@
-(Invoke-WebRequest -Uri https://install.python-poetry.org -UseBasicParsing).Content | python -
-
-$env:Path += ";$env:APPDATA\Python\Scripts"
-
-poetry --version
-poetry new --src .
-
